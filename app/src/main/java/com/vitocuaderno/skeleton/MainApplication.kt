b7ee5094f567b67497bc3f0c1f9d771d32a00bef
Anthony Vito Cuaderno <anthonyvitocuaderno@gmail.com>
2022-02-16 14:55:38 +0800

    Feat: add Firebase crashlytics

--- app/src/main/java/com/vitocuaderno/skeleton/MainApplication.kt
+++ app/src/main/java/com/vitocuaderno/skeleton/MainApplication.kt
@@ -7,6 +7,9 @@ import com.vitocuaderno.skeleton.data.remote.ApiService
 import dagger.hilt.android.HiltAndroidApp
 import javax.inject.Inject
 
+/***
+ * https://console.firebase.google.com/u/0/project/sample-kotlin-app-5f4d2/crashlytics/app/android:com.vitocuaderno.skeleton/issues?state=open&time=last-seven-days&tag=all
+ */
 @HiltAndroidApp
 class MainApplication : Application(), Configuration.Provider {
     @Inject
